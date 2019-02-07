c123e224bc25e42c0be3ba31f5e51640fc5dd890
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2019-02-07 08:12:16 -0800

    variable declarations

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log("Hello World");
 //Comments in JavaScript Code
     //in-line comment
     /*
@@ -6,4 +7,30 @@
          comment
     */
 
-    
+/** Data-types and Variables :
+     * undefined
+     * null
+     * boolean
+     * string
+     * symbol
+     * number
+     * object
+ **/    
+
+ /***
+  * 3 ways to declare variables:
+  *  1.var
+  *  2. let
+  * 3.const
+  */
+
+ var myname = "Rucha";  //You can var anywhere in the program
+
+ console.log(myname);
+ myname = "Ruchu";
+ console.log(myname);
+
+ let yourname="user"; //You can use let only where it is declared. i.e within the scope
+
+ 
+ const pi = 3.14;  //const is constant. You can never change its value. The name is contradictory because it does not vary once it is declared.
